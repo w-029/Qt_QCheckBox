255e8ad15e1c3911494c912ecd149b64376efa83
w-029 <w-029@163.com>
2019-01-02 22:40:21 +0800

    simulate setCheckable(false), but can setChecked(true)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,28 +2,55 @@
 #include "ui_mainwindow.h"
 #include <QFile>
 #include <QTextStream>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    /* apply the qss style */
-    QFile file(":/Qss/checkbox.qss");
-    if (file.open(QFile::ReadOnly)) {
-        QString fileText = file.readAll();
 
-        ui->QCheckBox_1->setChecked(true);
-        ui->QCheckBox_1->setStyleSheet(fileText);
-
-        ui->QCheckBox_2->setChecked(true);
-        ui->QCheckBox_2->setStyleSheet(fileText);
-
-        file.close();
-    }
+    setStyle();
+    connect(ui->QCheckBox_2, SIGNAL(clicked()), this, SLOT(on_clicked()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::setStyle()
+{
+    QFile file(":/Qss/res/checkbox.qss");
+    if (!file.exists()) {
+        qDebug() << "err: no qss file";
+        return;
+    }
+    if (!file.open(QFile::ReadOnly)) {
+        qDebug() << "err: fail to open qss file";
+        return;
+    }
+
+    QString fileText = file.readAll();
+
+    ui->QCheckBox_1->setChecked(true);
+    ui->QCheckBox_1->setStyleSheet(fileText);
+    /* this will make setChecked(false) effect */
+    ui->QCheckBox_1->setCheckable(false);
+
+    ui->QCheckBox_2->setChecked(true);
+    ui->QCheckBox_2->setStyleSheet(fileText);
+
+    file.close();
+}
+
+void MainWindow::on_clicked()
+{
+    /* simulate setCheckable(false), but can setChecked(true) */
+    if (ui->QCheckBox_2->isChecked()) {
+        ui->QCheckBox_2->setChecked(false);
+    }
+    else {
+        ui->QCheckBox_2->setChecked(true);
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,12 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_clicked();
+
+private:
+    void setStyle();
+
 private:
     Ui::MainWindow *ui;
 };
